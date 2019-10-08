b5857ff7114067e643e8847936d4751781867843
blackwellsmith <blackwellsmithiv@gmail.com>
2019-10-08 11:29:49 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,10 @@
 class ProductsController < ApplicationController
 
     def index
-        @products = Product.all 
     end
 
     def add
-        @product = Product.find(params[:id])
-        current_cart << @product.id
-        session[:cart] = cart
+        cart << params[:product]
+        render :index
     end
 end
